a938897800401340e81ae58ab145542cd4326564
bj600800 <bj600800@vip.qq.com>
2023-03-05 00:56:21 +0800

    build_cath_desc_complete

--- qprotein/database/build_cath_desc - cp.py
+++ qprotein/database/build_cath_desc - cp.py
@@ -20,76 +20,69 @@ class SqlBuilder(object):
         self.sql_db = sql_db
         self.table_name = table_name
         self.column_definition = column_definition
-
+        self.records = []
         self.main()
 
     def read_desc_file(self):
-        logger.info(f"Step 1 -> Read source file: {self.desc_file}.")
+        logger.info(f"Step 1 -> Read source file: {self.desc_file}")
 
         with open(self.desc_file, 'r') as f:
             desc_content = [line for line in f.readlines() if not line.startswith('#')]
         return desc_content
 
     def create_cath_sql(self):
-        logger.info(f"Step 2 -> Create SQL table: {self.table_name}.")
+        logger.info(f"Step 2 -> Create SQL table: {self.table_name}")
 
         connect = sqlite3.connect(self.sql_db)
         cursor = connect.cursor()
         cursor.execute(f"DROP TABLE IF EXISTS {self.table_name}")
         definition_str = ','.join([' '.join(i) for i in self.column_definition.items()])
         cursor.execute(f"CREATE TABLE {self.table_name} ({definition_str})")
-
         return cursor
 
-    def insert_data(self, desc_content, cursor):
-        logger.info(f"Step 3 -> Insert data to SQL table: {self.table_name}")
+    def pack_records(self, desc_content):
+        logger.info("Step 3 -> Pack records for SQL batch inserting")
+        # TODO: overwrite in subclass
+        # Different object has different data structure.
 
-        records = []
-        record_counter = 0
-        column_items = {}
-        reg = re.compile(r"  +")
+        record_items = {}
         prefixes = ["DOMAIN", "CATHCODE", "CLASS", "ARCH", "TOPOL", "HOMOL"]
+        reg = re.compile(r"  +")
 
         for line in desc_content:
-            if record_counter == 50000:
-                cursor.execute("begin")
-                cursor.executemany(f"INSERT INTO {self.table_name} VALUES(?, ?, ?, ?, ?, ?)", records)
-                cursor.execute("commit")
-                logger.info(f"Insert 50000 items.")
-                records = []
-                record_counter = 0
-                records.append(tuple(column_items.values()))
-                record_counter += 1
-            else:
-                for prefix in prefixes:
-                    if line.startswith(prefix):
-                        column_items[prefix] = reg.split(line)[1].rstrip()
-                if line.startswith("//\n"):
-                    records.append(tuple(column_items.values()))
-                    record_counter += 1
+            for prefix in prefixes:
+                if line.startswith(prefix):
+                    record_items[prefix] = reg.split(line)[1].rstrip()
+            if line.startswith("//\n"):
+                self.records.append(tuple(record_items.values()))
 
-        if record_counter > 0:
-            logger.info("Step 4 -> Insert the remaining records.")
-            cursor.execute("begin")
-            cursor.executemany(f"INSERT INTO {self.table_name} VALUES(?, ?, ?, ?, ?, ?)", records)
-            cursor.execute("commit")
+    def insert_batch(self, cursor, records: list):
+        logger.info(f"Step 4 -> Insert data in bulk to SQL table: {self.table_name}")
+        cursor.execute("begin")
+        cursor.executemany(f"INSERT INTO {self.table_name} VALUES(?, ?, ?, ?, ?, ?)", records)
+        cursor.execute("commit")
 
-        logger.info(f'Step 5 -> Creating index for SQL table: {self.table_name}.')
-
+    def create_index(self, cursor):
+        logger.info(f'Step 5 -> Creating index for SQL table: {self.table_name}')
         column_name = ', '.join(self.column_definition.keys())
         cursor.execute(f"CREATE INDEX cath_desc_idx ON cath_desc ({column_name})")
 
-        logger.info(f'Successfully build SQL database for {table_name}')
+    def exec_sql(self, desc_content, cursor):
+        self.pack_records(desc_content)
+        self.insert_batch(cursor, self.records)
+        self.create_index(cursor)
 
     def main(self):
+        logger.info(f'Start to build SQL database for {table_name}')
         desc_content = self.read_desc_file()
         cursor = self.create_cath_sql()
-        self.insert_data(desc_content, cursor)
+        self.exec_sql(desc_content, cursor)
+        logger.info(f'Successfully built SQL database for {table_name}')
 
 
 if __name__ == '__main__':
     desc_file = r'G:\DB\cath\cath-classification-data_txt\cath-domain-description-file.txt'
-    sql_db = r'G:\DB\cath\cath-classification-data_txt\cath.db'
+    sql_db = r'C:\Users\bj600\Desktop\cath.db'
     table_name = 'cath_desc'
     column_definition = {"domain_name": "TEXT", "cath_code": "TEXT",
                          "cath_class": "TEXT", "cath_architecture": "TEXT",
